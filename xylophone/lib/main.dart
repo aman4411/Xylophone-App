88c989aac37fe98fad196d732f341fa2d1be25cf
aman4411 <amanmehta4411@gmail.com>
2020-04-15 17:24:33 +0530

    UI updated

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -14,14 +14,20 @@ class XylophoneApp extends StatelessWidget {
     }
 
     //build sound keys function
-    Expanded buildKey({Color keyColor, int soundNumber}) {
+    Expanded buildKey(
+        {Color color1, Color color2, Color color3, int soundNumber}) {
       return Expanded(
-        child: FlatButton(
-          child: null,
-          color: keyColor,
-          onPressed: () {
-            playSound(soundNumber);
-          },
+        child: Container(
+          decoration: BoxDecoration(
+              gradient:
+                  LinearGradient(colors: <Color>[color1, color2, color3])),
+          child: FlatButton(
+            child: null,
+            // color: keyColor,
+            onPressed: () {
+              playSound(soundNumber);
+            },
+          ),
         ),
       );
     }
@@ -33,13 +39,41 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(keyColor: Colors.lightBlue, soundNumber: 1),
-              buildKey(keyColor: Colors.lime, soundNumber: 2),
-              buildKey(keyColor: Colors.pinkAccent, soundNumber: 3),
-              buildKey(keyColor: Colors.lightGreen, soundNumber: 4),
-              buildKey(keyColor: Colors.yellow, soundNumber: 5),
-              buildKey(keyColor: Colors.orange, soundNumber: 6),
-              buildKey(keyColor: Colors.red, soundNumber: 7),
+              buildKey(
+                  color1: Colors.deepPurple,
+                  color2: Colors.deepPurple[300],
+                  color3: Colors.deepPurple[100],
+                  soundNumber: 5),
+              buildKey(
+                  color1: Colors.red,
+                  color2: Colors.red[300],
+                  color3: Colors.red[100],
+                  soundNumber: 2),
+              buildKey(
+                  color1: Colors.blue[600],
+                  color2: Colors.blue[300],
+                  color3: Colors.blue[100],
+                  soundNumber: 1),
+              buildKey(
+                  color1: Colors.green,
+                  color2: Colors.green[300],
+                  color3: Colors.green[100],
+                  soundNumber: 3),
+              buildKey(
+                  color1: Colors.yellow,
+                  color2: Colors.yellow[300],
+                  color3: Colors.yellow[100],
+                  soundNumber: 4),
+              buildKey(
+                  color1: Colors.pink,
+                  color2: Colors.pink[300],
+                  color3: Colors.pink[100],
+                  soundNumber: 6),
+              buildKey(
+                  color1: Colors.blueGrey,
+                  color2: Colors.blueGrey[300],
+                  color3: Colors.blueGrey[100],
+                  soundNumber: 7),
             ],
           ),
         ),
